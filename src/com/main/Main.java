874d0458aa78617bd6aa23c60152207c322d294a
lathieuhiep <khacdiepkma90@gmail.com>
2024-08-30 15:34:42 +0700

    update code

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -6,9 +6,10 @@ public class Main {
     public static void main(String[] args) {
         RSSParse rssParse = new RSSParse();
 
-        String link = "https://drive.google.com/file/d/1OVsBLrL4b3k_Wxgm_miRiH7RNx6rzVDp/view?usp=share_link";
+        String link = "https://drive.usercontent.google.com/download?id=1OVsBLrL4b3k_Wxgm_miRiH7RNx6rzVDp&export=download&authuser=0";
         String savePath = "D:/java-exercises/bai-tap-buoi-19/downloads";
 
         rssParse.download(link, savePath);
+        rssParse.parse(savePath + "/trangchu.rss");
     }
 }
--- src/com/rss/RSSParse.java
+++ src/com/rss/RSSParse.java
@@ -1,12 +1,15 @@
 package com.rss;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RSSParse {
     private ArrayList<ItemNews> listNews = new ArrayList<>();
@@ -14,10 +17,11 @@ public class RSSParse {
     public void download(String link, String savePath) {
         try {
             URLConnection conn = new URL(link).openConnection();
+            int fileSize = conn.getContentLength();
             InputStream in = conn.getInputStream();
 
-            // lay ten file
-            String fileName = new File(link).getName();
+            // dat ten file
+            String fileName = "trangchu.rss";
 
             // tao thu muc
             new File(savePath).mkdirs();
@@ -26,11 +30,25 @@ public class RSSParse {
             FileOutputStream out = new FileOutputStream(f2);
 
             byte[] gioByte = new  byte[1024];
-
             int slByte = in.read(gioByte);
+            int downloadedSize = 0;
+            int lastPercent = 0;
+
             while ( slByte > 0 ) {
                 // ghi luon file moi
                 out.write(gioByte, 0, slByte);
+
+                // tính toán phần trăm tải
+                downloadedSize += slByte;
+                int percent = (int) (((double) downloadedSize / fileSize) * 100);
+
+                while (lastPercent < percent) {
+                    lastPercent++;
+                    System.out.print("Download Progress: " + lastPercent + "%\r");
+
+                    Thread.sleep(10);
+                }
+
                 slByte = in.read(gioByte);
             }
 
@@ -41,5 +59,113 @@ public class RSSParse {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void parse(String path) {
+        File file = new File(path);
+        StringBuilder text = new StringBuilder();
+
+        try {
+            if ( !file.exists() ) {
+                System.out.println("File không tồn tại");
+                return;
+            }
+
+            FileInputStream in = new FileInputStream(file);
+            byte[] gioByte = new byte[1024];
+            int soLuongByte = in.read(gioByte);
+
+            while (soLuongByte > 0) {
+                String chuoi = new String(gioByte, 0, soLuongByte);
+                text.append(chuoi);
+
+                soLuongByte = in.read(gioByte);
+            }
+
+            ArrayList<ItemNews> newsList = new ArrayList<>();
+            if ( !text.isEmpty() ) {
+                ArrayList<String> items = getItems(text);
+
+                if ( !items.isEmpty() ) {
+                    String title = "";
+                    String tagDescription = "";
+                    String linkImg = "";
+                    String description = "";
+                    String linkPost = "";
+                    String date = "";
+
+                   for ( String item : items ) {
+                       if (item.contains("<title>") && item.contains("</title>")) {
+                           title = item.substring(item.indexOf("<title>") + 7, item.indexOf("</title>"));
+                       }
+
+                       if (item.contains("<description>") && item.contains("</description>")) {
+                           tagDescription = item.substring(item.indexOf("<description>") + 13, item.indexOf("</description>"));
+
+                           // Lấy linkImg từ tagDescription
+                           if (tagDescription.contains("<img src=\"")) {
+                               int imgStartIndex = tagDescription.indexOf("<img src=\"") + 10;
+                               int imgEndIndex = tagDescription.indexOf("\"", imgStartIndex);
+
+                               if (imgStartIndex > 9 && imgEndIndex > imgStartIndex) {
+                                   linkImg = tagDescription.substring(imgStartIndex, imgEndIndex);
+                               }
+                           }
+
+                           // Lấy phần mô tả sau thẻ img
+                           if (tagDescription.contains("/>")) {
+                               description = tagDescription.substring(tagDescription.indexOf("/>") + 2).trim();
+                           }
+                       }
+
+                       // Kiểm tra và lấy linkPost
+                       if (item.contains("<link>") && item.contains("</link>")) {
+                           linkPost = item.substring(item.indexOf("<link>") + 6, item.indexOf("</link>"));
+                       }
+
+                       // Kiểm tra và lấy date
+                       if (item.contains("<pubDate>") && item.contains("</pubDate>")) {
+                           date = item.substring(item.indexOf("<pubDate>") + 9, item.indexOf("</pubDate>"));
+                       }
+
+                       newsList.add( new ItemNews(title, linkImg, description, linkPost, date) );
+                   }
+                }
+            }
+
+            if ( !newsList.isEmpty() ) {
+                System.out.println("-----------------------------------");
+
+                for (ItemNews itemNews : newsList) {
+                    System.out.println(itemNews);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static ArrayList<String> getItems(StringBuilder text) {
+        ArrayList<String> items = new ArrayList<>();
+
+        String textToString = text.toString();
+        String itemTagStart = "<item>";
+        String itemTagEnd = "</item>";
+
+        int start = 0;
+        while ( (start = textToString.indexOf(itemTagStart, start)) != -1 ) {
+            int end = textToString.indexOf(itemTagEnd, start) + itemTagEnd.length();
+
+            String item = textToString.substring(start, end).trim();
+
+            String content = item.substring(itemTagStart.length(), item.length() - itemTagEnd.length()).trim();
+
+            items.add(content);
+
+            start = end;
+        }
+
+        return items;
     }
 }
